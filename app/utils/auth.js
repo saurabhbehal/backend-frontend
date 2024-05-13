9f697d367a2330901f6eac1b266950cf9f4fc324
Abhishek kumar <abhisec.tech@gmail.com>
2024-05-13 03:05:37 -0700

    site migrated

--- app/utils/auth.js
+++
@@ -1,8 +0,0 @@
-// auth.js
-
-export const isAuthenticated = () => {
-    // Check if the user is authenticated
-    const isLoggedIn = localStorage.getItem('isLoggedIn');
-    return isLoggedIn === 'true';
-  };
-  
